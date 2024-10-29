e38d0213fbc919f784887269b27dbe61b72bc2f7
Matt Larsen <matthew.larsen13@gmail.com>
2024-10-29 12:40:48 -0400

    Oct 29

--- react-layout/src/Layout.js
+++ react-layout/src/Layout.js
@@ -1,15 +1,25 @@
 import {Outlet, Link} from "react-router-dom";
+import {useState} from "react";
 import Header from "./components/Header";
-
+import "./css/Layout.css"
 
 const Layout = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return(
         <>
             <Header />
+            <button onClick={toggleMenu}>Toggle</button>
 
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/pictures">Pictures</Link>
+            <nav id="main-nav" className={menuOpen?"":"hide-small"}>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/pictures">Pictures</Link></li>
+            </nav>
 
             <Outlet />
 
